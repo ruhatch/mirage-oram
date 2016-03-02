6df46356fba6d7c4e2e9cac9947185468dec5c89
ruhatch <ruhatch@me.com>
2016-03-02 11:25:42 +0000

    Prepared encrypted time experiment

---
+++ tests/encryptedTimeExperiment.ml
@@ -0,0 +1,77 @@
+open Core.Std
+
+module B = BlockSizeController.Make(Block)
+module E = Block_ccm.Make(B)
+module O = Oram.Make(PosMap.InMemory)(E)
+
+let ( >>= ) x f = Lwt.bind x @@ function
+  | `Error e -> Lwt.return (`Error e)
+  | `Ok x -> f x
+
+let connectAndInitialiseORAMOfSize desiredSizeInSectors desiredBlockSize =
+  match Lwt_main.run (
+            Block.connect (Printf.sprintf "diskEnc%Ld.img" desiredSizeInSectors) >>= fun bd ->
+            B.connect ~desiredBlockSize:(desiredBlockSize * 2) bd >>= fun bd ->
+            E.connect bd ~key:(Cstruct.of_string "keyofsixteenchar") >>= fun bd ->
+            O.create ~desiredSizeInSectors ~desiredBlockSize bd) with
+  | `Ok oram -> oram
+  | `Error _ -> failwith (Printf.sprintf "Failed to connect to oram with size %Ld" desiredSizeInSectors)
+
+let connectToORAM desiredSizeInSectors desiredBlockSize =
+  match Lwt_main.run (
+            Block.connect (Printf.sprintf "diskEnc%Ld.img" desiredSizeInSectors) >>= fun bd ->
+            B.connect ~desiredBlockSize:(desiredBlockSize * 2) bd >>= fun bd ->
+            E.connect bd ~key:(Cstruct.of_string "keyofsixteenchar") >>= fun bd ->
+            O.connect bd) with
+  | `Ok oram -> oram
+  | `Error _ -> failwith (Printf.sprintf "Failed to connect to oram with size %Ld" desiredSizeInSectors)
+
+let dataForORAM oram =
+  let info = Lwt_main.run (O.get_info oram) in
+  Cstruct.create (info.O.sector_size)
+
+let desiredSizes minHeight maxHeight =
+  let heights = List.range minHeight maxHeight in
+  List.map ~f:(fun height -> Int64.of_int ((Int.pow 2 (height + 1) - 1) * 4)) heights
+
+let performExperiment oram desiredSizeInSectors data iterations =
+  let rec loopWrite = function
+    | 0 -> Lwt.return (`Ok ())
+    | n ->
+      O.write oram 0L [data] >>= fun () ->
+      loopRead (n - 1)
+  and loopRead = function
+  | 0 -> Lwt.return (`Ok ())
+  | n ->
+    O.read oram 0L [data] >>= fun () ->
+    loopWrite (n - 1)
+  in loopWrite iterations
+
+let () =
+  Command.basic
+    ~summary:"Run n iterations of Access on ORAMs with heights between x and y"
+    Command.Spec.(
+    empty
+    +> anon ("iterations" %: int)
+    +> anon ("minHeight" %: int)
+    +> anon ("maxHeight" %: int)
+    +> flag "i" no_arg ~doc:"Pass in this flag to initialise block devices before connecting"
+    +> flag "b" (optional_with_default 1048576 int) ~doc:"Optional block size parameter")
+    (fun iterations minHeight maxHeight shouldInitialise desiredBlockSize () ->
+      ignore @@ Nocrypto_entropy_lwt.initialize ();
+      List.iter (desiredSizes minHeight maxHeight)
+                ~f:(fun desiredSizeInSectors ->
+                  Printf.printf "%Ld, %!" desiredSizeInSectors;
+                  let oram =
+                    if shouldInitialise
+                    then connectAndInitialiseORAMOfSize desiredSizeInSectors desiredBlockSize
+                    else connectToORAM desiredSizeInSectors desiredBlockSize in
+                  let data = dataForORAM oram in
+                  let start = Time_ns.now () in
+                  begin match Lwt_main.run (performExperiment oram desiredSizeInSectors data iterations) with
+                  | `Ok () -> ()
+                  | `Error _ -> failwith "Failed to perform experiment"
+                  end;
+                  let time = Time_ns.abs_diff start (Time_ns.now ()) in
+                  Printf.printf "%f\n%!" (Time_ns.Span.to_ms time)))
+  |> Command.run
