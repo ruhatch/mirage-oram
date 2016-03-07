13ac05f5ed5ae2ec1cf2e456e5f3bafe7b89a078
ruhatch <ruhatch@me.com>
2016-03-07 08:02:47 +0000

    Added oram creation print statement

--- tests/run10TimeExperiments.sh
+++ tests/run10TimeExperiments.sh
@@ -1,5 +1,5 @@
 let i
 
 for (( i = 0; i < 10; i++ )); do
-  ~/mirage-oram/oramMacroBench.native 1000 1 11 > "timeResults$i.dat" 2> "timeResults$i.err";
+  ~/mirage-oram/oramMacroBench.native 1000 1 11 > "timeResults$i.dat";
 done
